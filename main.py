55ed4fe3483278fff6ce3466fe3ad84196b05e67
DonMins <maks000156@yandex.ru>
2020-04-25 12:14:30 +0400

    ну....

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-def addNoise (image,noise_percentage):
+
+def addNoise(image, noise_percentage):
     vals = len(image.flatten())
     out = np.copy(image)
     nose = int(np.ceil(noise_percentage * vals / 100))
     # Salt mode
-    num_salt = int(nose/2)
-    num_pepper = int(nose/2)
+    num_salt = int(nose / 2)
+    num_pepper = int(nose / 2)
     coords = [np.random.randint(0, i - 1, int(num_salt))
               for i in image.shape]
     out[coords] = 1
@@ -19,54 +20,101 @@ def addNoise (image,noise_percentage):
     out[coords] = 0
     return out
 
+
 def getVec_b(img, imgNose):
     b = np.zeros(16)
     i = 0
-    for n in range(-1,3):
-        for m in range(-1,3):
+    for n in range(-1, 3):
+        for m in range(-1, 3):
             b[i] = B(img, imgNose, -n, -m)
-            i +=1
+            i += 1
     return b
 
+
+def matrixA(imgNose):
+    matrixA = []
+    for n in range(-1, 3):
+        for m in range(-1, 3):
+             for k in range(-1, 3):
+                 for l in range(-1, 3):
+                     matrixA.append(B(imgNose, imgNose, n - k, m - l))
+
+    matrixA = np.array(matrixA).reshape(16,16)
+
+    return matrixA
+
+def matrixMask(A,b):
+    return np.linalg.solve(A,b)
 
 def B(A, B, k, l):
     rows = A.shape[0]
     cols = A.shape[1]
-    rowsA = range(k, rows )
-    colsA = range(l, cols )
-    rowsB = range(0, rows - k )
-    colsB = range(0, cols - l )
+    rowsA = range(k, rows)
+    colsA = range(l, cols)
+    rowsB = range(0, rows - k)
+    colsB = range(0, cols - l)
     if k < 0:
         k = abs(k)
-        rowsA = range(0, rows - k )
+        rowsA = range(0, rows - k)
         rowsB = range(k, rows)
     if l < 0:
         l = abs(l)
-        colsA = range(0, cols - l )
-        colsB = range(l, cols )
+        colsA = range(0, cols - l)
+        colsB = range(l, cols)
 
-    return np.sum(np.sum(A[rowsA, colsA] * B[rowsB, colsB])) / ((rows - 1) * (cols - 1))
+    return np.sum(np.sum(A[rowsA][colsA] * B[rowsB][colsB])) / ((rows - 1) * (cols - 1))
 
 
+def Convolution(image, kernel):
+    heightM, widthM = kernel.shape
+    height, width = image.shape
+
+    centerHeightM = int(np.ceil(heightM / 2) - 1)
+    centerwidthM = int(np.ceil(widthM / 2) - 1)
+
+    img2 = np.zeros((height + centerHeightM * 2, width + centerwidthM * 2), np.uint8)
+    img2[centerHeightM:height + centerHeightM, centerwidthM:width + centerwidthM] = image
+
+    for i in range(centerHeightM):
+        img2[i, :] = img2[centerHeightM, :]
+        img2[height + 1 + i, :] = img2[height, :]
+
+    for i in range(centerwidthM):
+        img2[:, i] = img2[:, centerwidthM]
+        img2[:, width + 1 + i] = img2[:, width]
+
+    new_image = np.zeros((height + centerHeightM, width + centerwidthM))
+
+    for i in range(centerHeightM, height + centerHeightM):
+        for j in range(centerwidthM, width + centerwidthM):
+            new_image[i - centerHeightM][j - centerwidthM] = np.sum(img2[i - centerHeightM: i + centerHeightM + 1,
+                                                                    j - centerwidthM: j + centerwidthM + 1] * kernel)
+
+    return new_image[centerHeightM:height + centerHeightM, centerwidthM:width + centerwidthM]
 
 if __name__ == '__main__':
     img = cv2.imread("putin.jpg")
     imgNose = cv2.imread("putinNose.jpg")
 
     b, g, r = cv2.split(img)
+    bNose, gNose, rNose = cv2.split(imgNose)
 
     # imgNose = addNoise(img,13)
 
     # cv2.imwrite("putinNose.jpg",imgNose)
-    bNose, gNose, rNose = cv2.split(imgNose)
 
-    print(getVec_b(img,imgNose))
+    bvec = getVec_b(b, bNose)
+    A = matrixA(bNose)
+    Mask = matrixMask(A,bvec)
+    Mask = np.array(Mask).reshape(4,4)
 
+    result = Convolution(bNose,Mask)
 
+    cv2.imshow("Approval Method", result)
 
+    cv2.waitKey(0)
     img2 = cv2.imread("tramp.jpg")
     b2, g2, r2 = cv2.split(img)
 
     img3 = cv2.imread("car.jpg")
     b3, g3, r3 = cv2.split(img)
-
